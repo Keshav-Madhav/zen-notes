a188278ac99de0b743636330de7a7d01b5d86bc4
Keshav Madhav <keshav2552003@gmail.com>
2024-08-06 20:07:28 +0530

    Added clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="">
       <h1>AI APP</h1>
+
+      <Button>Click me</Button>
     </main>
   );
 }
