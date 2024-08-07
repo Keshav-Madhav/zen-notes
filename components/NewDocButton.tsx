559f53521c7b8943d51e181a4822790344fe28ae
Keshav Madhav <keshav2552003@gmail.com>
2024-08-07 12:36:32 +0530

    Added database for users an collections and functionality of new document

--- components/NewDocButton.tsx
+++ components/NewDocButton.tsx
@@ -1,10 +1,31 @@
+'use client'
+
+import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
+import { useTransition } from "react"
+import { createNewDocument } from "@/actions/actions"
 
 type Props = {}
 
 const NewDocButton = (props: Props) => {
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+
+  const handleCreate = () => {
+    startTransition(async() => {
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`)
+    })
+  }
+
   return (
-    <Button className="w-full">New Document</Button>
+    <Button 
+      className="w-full"
+      onClick={handleCreate}
+      disabled={isPending}
+    >
+      {isPending ? "Creating..." : "New Document"}
+    </Button>
   )
 }
 
